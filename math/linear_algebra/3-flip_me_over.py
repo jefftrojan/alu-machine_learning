88a6824abc741151a616e60b8bf85921d48fb3c1
jefftrojan <j.dauda@alustudent.com>
2024-02-02 03:51:32 +0200

    Flip me over

--- math/linear_algebra/3-flip_me_over.py
+++ math/linear_algebra/3-flip_me_over.py
@@ -6,5 +6,7 @@ def matrix_transpose(matrix):
     """
     Returns the transpose of a 2D matrix
     """
-    return [[matrix[j][i] for j in range(len(matrix))]
-for i in range(len(matrix[0]))]
+    transpose = [
+        [matrix[j][i] for j in range(len(matrix))]
+        for i in range(len(matrix[0]))
+    ]

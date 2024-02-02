b52045e71d0a4607c77d1fa5679fcd441fcce42b
jefftrojan <j.dauda@alustudent.com>
2024-02-02 02:31:45 +0200

    fix pep validation

--- math/linear_algebra/0-slice_me_up.py
+++ math/linear_algebra/0-slice_me_up.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python3
+# Create a slice of the array that contains the first two numbers of the array
+# Create a slice of the array that contains the last five numbers of the array
+# Create a slice of the array that contains the 2nd through 6th numbers of the array
+
+
 arr = [9, 8, 2, 3, 9, 4, 1, 0, 3]
 arr1 =  arr[0:2]
 arr2 =  arr[-5:]

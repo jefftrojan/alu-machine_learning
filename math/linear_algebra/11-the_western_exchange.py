f0082780d5af928512c25a38165243d31c999c4a
jefftrojan <j.dauda@alustudent.com>
2024-02-02 04:46:04 +0200

    transpose 2d matrix

---
+++ math/linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+transpose an n x m matrix (2D list) and return the transposed matrix.
+"""
+
+
+def np_transpose(matrix):
+    """
+    Returns the transpose of a 2D matrix
+    """
+    return matrix.transpose()

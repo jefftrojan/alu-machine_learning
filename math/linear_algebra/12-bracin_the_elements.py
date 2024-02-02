b452aeddd4a2e4682058b1af499a5f9ed01cd2c7
jefftrojan <j.dauda@alustudent.com>
2024-02-02 04:50:05 +0200

    Element wise operations

---
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Bracing the elements
+"""
+
+
+def np_elementwise(mat1, mat2):
+    """
+    Adds, subtracts, multiplies, and divides two matrices element-wise
+    """
+    return tuple(mat1 + mat2, mat1 - mat2, mat1 * mat2, mat1 / mat2)

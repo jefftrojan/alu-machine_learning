2319a2130ecb5fa8fa45eb4de1845b8bcf7408cf
jefftrojan <j.dauda@alustudent.com>
2024-02-02 02:54:24 +0200

    1-trim

---
+++ math/linear_algebra/1-trim_me_down.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+matrix = [[1, 3, 9, 4, 5, 8], [2, 4, 7, 3, 4, 0], [0, 3, 4, 6, 1, 5]]
+the_middle = []
+the_middle = [row[2:4] for row in matrix]
+print("The middle columns of the matrix are: {}".format(the_middle))

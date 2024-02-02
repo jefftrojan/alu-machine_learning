925d820fbac1a9ba0931dba81a615c9c23dd7666
jefftrojan <j.dauda@alustudent.com>
2024-02-02 03:57:37 +0200

    Add arrays

---
+++ math/linear_algebra/4-line_up.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Adds two arrays element-wise
+"""
+
+
+def add_arrays(arr1, arr2):
+    """
+    Adds two arrays element-wise
+    """
+    if len(arr1) != len(arr2):
+        return None
+    return [arr1[i] + arr2[i] for i in range(len(arr1))]

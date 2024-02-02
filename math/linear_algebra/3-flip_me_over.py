b5b5c86474634ee6f38e8856d4d49b39775d9cb1
jefftrojan <j.dauda@alustudent.com>
2024-02-02 03:45:28 +0200

    Flip me over

---
+++ math/linear_algebra/3-flip_me_over.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" flip who over ? """
+
+
+def matrix_transpose(matrix):
+    """
+    Returns the transpose of a 2D matrix
+    """
+    return [[matrix[j][i] for j in range(len(matrix))] for i in range(len(matrix[0]))]

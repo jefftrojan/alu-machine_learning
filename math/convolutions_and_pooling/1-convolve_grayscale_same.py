c432a1bd26d592608218cd8189df6ac1fb0002d3
jefftrojan <j.dauda@alustudent.com>
2024-02-24 23:55:53 +0300

    Task 1

---
+++ math/convolutions_and_pooling/1-convolve_grayscale_same.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+"""Function that performs a valid convolution on grayscale images"""
+
+import numpy as np
+
+
+def convolve_grayscale_same(images, kernel):
+    """Performs a valid convolution on grayscale images
+    Args:
+        images: `numpy.ndarray` with shape (m, h, w)
+            containing multiple grayscale images
+            m: `int`, is the number of images
+            h: `int`, is the height in pixels of the images
+            w: `int`, is the width in pixels of the images
+        kernel: `numpy.ndarray` with shape (kh, kw)
+            containing the kernel for the convolution
+            kh: `int`, is the height of the kernel
+            kw: `int`, is the width of the kernel
+    Returns:
+         output: `numpy.ndarray` containing the convolved images
+    """
+    m, h, w = images.shape[0], images.shape[1], images.shape[2]
+    kh, kw = kernel.shape[0], kernel.shape[1]
+    pw = int(kw / 2)
+    ph = int(kh / 2)
+    convolved = np.zeros((m, h, w))
+    npad = ((0, 0), (ph, ph), (pw, pw))
+    imagesp = np.pad(images, pad_width=npad,
+                     mode='constant', constant_values=0)
+    for i in range(h):
+        for j in range(w):
+            image = imagesp[:, i:i + kh, j:j + kw]
+            convolved[:, i, j] = np.sum(np.multiply(image, kernel),
+                                        axis=(1, 2))
+    return convolved

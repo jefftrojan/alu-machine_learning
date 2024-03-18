39925f117927a37f6c6e643d6b2bdde64c76d08d
jefftrojan <j.dauda@alustudent.com>
2024-03-18 10:58:10 +0300

    3 posterior

---
+++ math/bayesian_prob/3-posterior.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+""""Function  that calculates the posterior
+probability for the various hypothetical probabilities
+of developing severe side effects given the data"""
+
+import numpy as np
+
+
+def posterior(x, n, P, Pr):
+    """""Function  that calculates the posterior
+    probability for the various hypothetical probabilities
+    of developing severe side effects given the data"""
+    if type(n) is not int or n <= 0:
+        raise ValueError("n must be a positive integer")
+    if type(x) is not int or x < 0:
+        text = "x must be an integer that is greater than or equal to 0"
+        raise ValueError(text)
+    if x > n:
+        raise ValueError("x cannot be greater than n")
+    if (not isinstance(P, np.ndarray)) or len(P.shape) != 1:
+        raise TypeError("P must be a 1D numpy.ndarray")
+    if (not isinstance(Pr, np.ndarray)) or Pr.shape != P.shape:
+        raise TypeError("Pr must be a numpy.ndarray with the same shape as P")
+    if np.any(P < 0) or np.any(P > 1):
+        raise ValueError("All values in P must be in the range [0, 1]")
+    if np.any(Pr < 0) or np.any(Pr > 1):
+        raise ValueError("All values in Pr must be in the range [0, 1]")
+    if not np.isclose([np.sum(Pr)], [1.])[0]:
+        raise ValueError("Pr must sum to 1")
+    num = np.math.factorial(n)
+    den = np.math.factorial(x) * np.math.factorial(n - x)
+    coeficient = num / den
+    likehood = coeficient * (P ** x) * ((1 - P) ** (n - x))
+    intersection = likehood * Pr
+    return intersection / np.sum(intersection)

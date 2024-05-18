a90261fc80170459b1fe571e2363919c0a74079e
jefftrojan <j.dauda@alustudent.com>
2024-05-18 12:54:04 +0200

    Added file 17-deep_neural_network.py

---
+++ supervised_learning/classification/17-deep_neural_network.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+""" Deep Neural Network
+"""
+
+import numpy as np
+
+
+class DeepNeuralNetwork:
+    """ Class that defines a deep neural network performing binary
+        classification.
+    """
+
+    def __init__(self, nx, layers):
+        """ Instantiation function
+
+        Args:
+            nx (int): number of input features
+            layers (list): representing the number of nodes in each layer of
+                           the network
+        """
+        if not isinstance(nx, int):
+            raise TypeError('nx must be an integer')
+        if nx < 1:
+            raise ValueError('nx must be a positive integer')
+
+        if not isinstance(layers, list):
+            raise TypeError('layers must be a list of positive integers')
+        if len(layers) < 1:
+            raise TypeError('layers must be a list of positive integers')
+
+        self.__L = len(layers)
+        self.__cache = {}
+        self.__weights = {}
+
+        for i in range(self.__L):
+            if not isinstance(layers[i], int) or layers[i] < 1:
+                raise TypeError('layers must be a list of positive integers')
+
+            if i == 0:
+                # He et al. initialization
+                self.__weights['W' + str(i + 1)] = np.random.randn(
+                    layers[i], nx) * np.sqrt(2 / nx)
+            else:
+                # He et al. initialization
+                self.__weights['W' + str(i + 1)] = np.random.randn(
+                    layers[i], layers[i - 1]) * np.sqrt(2 / layers[i - 1])
+
+            # Zero initialization
+            self.__weights['b' + str(i + 1)] = np.zeros((layers[i], 1))
+
+    # add getter method
+    @property
+    def L(self):
+        """ Return layers in the neural network"""
+        return self.__L
+
+    @property
+    def cache(self):
+        """ Return dictionary with intermediate values of the network"""
+        return self.__cache
+
+    @property
+    def weights(self):
+        """Return weights and bias dictionary"""
+        return self.__weights

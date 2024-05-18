05afe5a711666135efeb4f6a0819aae4912bab63
jefftrojan <j.dauda@alustudent.com>
2024-05-18 12:54:07 +0200

    Added file 5-neuron.py

---
+++ supervised_learning/classification/5-neuron.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python3
+"""Class Neuron that defines a single neuron performing binary classification
+"""
+
+
+import numpy as np
+
+
+class Neuron:
+    """ Class Neuron
+    """
+
+    def __init__(self, nx):
+        """ Instantiation function of the neuron
+
+        Args:
+            nx (int): number of features to be initialized
+
+        Raises:
+            TypeError: _description_
+            ValueError: _description_
+        """
+        if not isinstance(nx, int):
+            raise TypeError('nx must be an integer')
+        if nx < 1:
+            raise ValueError('nx must be positive')
+
+        # initialize private instance attributes
+        self.__W = np.random.normal(size=(1, nx))
+        self.__b = 0
+        self.__A = 0
+
+        # getter function
+    @property
+    def W(self):
+        """Return weights"""
+        return self.__W
+
+    @property
+    def b(self):
+        """Return bias"""
+        return self.__b
+
+    @property
+    def A(self):
+        """Return output"""
+        return self.__A
+
+    def forward_prop(self, X):
+        """Calculates the forward propagation of the neuron
+
+        Args:
+            X (numpy.ndarray): matrix with the input data of shape (nx, m)
+
+        Returns:
+            numpy.ndarray: The output of the neural network.
+        """
+        z = np.matmul(self.__W, X) + self.__b
+        sigmoid = 1 / (1 + np.exp(-z))
+        self.__A = sigmoid
+        return self.__A
+
+    def cost(self, Y, A):
+        """ Compute the of the model using logistic regression
+
+        Args:
+            Y (np.array): True values
+            A (np.array): Prediction valuesss
+
+        Returns:
+            float: cost function
+        """
+        # calculate
+        loss = - (Y * np.log(A) + (1 - Y) * np.log(1.0000001 - A))
+        cost = np.mean(loss)
+        return cost
+
+    def evaluate(self, X, Y):
+        """ Evaluate the cost function
+
+        Args:
+            X (_type_): _description_
+            Y (_type_): _description_
+
+        Returns:
+            _type_: _description_
+        """
+        pred = self.forward_prop(X)
+        cost = self.cost(Y, pred)
+        pred = np.where(pred > 0.5, 1, 0)
+        return (pred, cost)
+
+    def gradient_descent(self, X, Y, A, alpha=0.05):
+        """ Calculate one pass of gradient descent on the neuron
+
+        Args:
+            X (_type_): _description_
+            Y (_type_): _description_
+            A (_type_): _description_
+            alpha (float, optional): _description_. Defaults to 0.05.
+        """
+        dz = A - Y
+        m = X.shape[1]
+        dw = (1/m) * np.matmul(dz, X.T)
+        db = np.mean(dz)
+        self.__W -= alpha * dw
+        self.__b -= alpha * db

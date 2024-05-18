8bc59427eda490c8ea1d61e8b26ff8a054ee1293
jefftrojan <j.dauda@alustudent.com>
2024-05-18 12:54:03 +0200

    Added file 12-neural_network.py

---
+++ supervised_learning/classification/12-neural_network.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python3
+""" Neural Network
+"""
+
+import numpy as np
+
+
+class NeuralNetwork:
+    """ Class that defines a neural network with one hidden layer performing
+        binary classification.
+    """
+
+    def __init__(self, nx, nodes):
+        """ Instantiation function
+
+        Args:
+            nx (int): size of the input layer
+            nodes (_type_): _description_
+        """
+        if not isinstance(nx, int):
+            raise TypeError('nx must be an integer')
+        if nx < 1:
+            raise ValueError('nx must be a positive integer')
+
+        if not isinstance(nodes, int):
+            raise TypeError('nodes must be an integer')
+        if nodes < 1:
+            raise ValueError('nodes must be a positive integer')
+
+        self.__W1 = np.random.randn(nodes, nx)
+        self.__b1 = np.zeros((nodes, 1))
+        self.__A1 = 0
+        self.__W2 = np.random.randn(1, nodes)
+        self.__b2 = 0
+        self.__A2 = 0
+
+    # getter functions
+    @property
+    def W1(self):
+        """Return weights vector for hidden layer"""
+        return self.__W1
+
+    @property
+    def b1(self):
+        """Return bias for hidden layer"""
+        return self.__b1
+
+    @property
+    def A1(self):
+        """Return activated output for hidden layer"""
+        return self.__A1
+
+    @property
+    def W2(self):
+        """Return weights vector for output neuron"""
+        return self.__W2
+
+    @property
+    def b2(self):
+        """Return bias for the output neuron"""
+        return self.__b2
+
+    @property
+    def A2(self):
+        """Return activated output for the output neuron"""
+        return self.__A2
+
+    def forward_prop(self, X):
+        """ Calculates the forward propagation of the neural network
+
+        Args:
+            X (numpy.array): Input data with shape (nx, m)
+        """
+        z = np.matmul(self.__W1, X) + self.__b1
+        sigmoid = 1 / (1 + np.exp(-z))
+        self.__A1 = sigmoid
+        z = np.matmul(self.__W2, self.__A1) + self.__b2
+        sigmoid = 1 / (1 + np.exp(-z))
+        self.__A2 = sigmoid
+        return self.__A1, self.__A2
+
+    def cost(self, Y, A):
+        """ Calculates the cost of the model using logistic regression
+
+        Args:
+            Y (_type_): _description_
+            A (_type_): _description_
+        """
+        loss = -(Y * np.log(A) + (1 - Y) * np.log(1.0000001 - A))
+        cost = np.mean(loss)
+        return cost
+
+    def evaluate(self, X, Y):
+        """ Evaluates the neural network’s predictions
+
+        Args:
+            X (_type_): _description_
+            Y (_type_): _description_
+        """
+        self.forward_prop(X)
+        return np.where(self.__A2 >= 0.5, 1, 0), self.cost(Y, self.__A2)

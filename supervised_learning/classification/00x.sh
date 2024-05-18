9e95bc6c64531de27711584db395e54d70e32dfc
jefftrojan <j.dauda@alustudent.com>
2024-05-18 12:55:10 +0200

    Added file 00x.sh

--- supervised_learning/classification/00x.sh
+++ supervised_learning/classification/00x.sh
@@ -1,22 +1,19 @@
 #!/bin/bash
 
-# Check if there are any files in the directory
+# check if there are any files in the dir
 if [ -z "$(ls -A .)" ]; then
   echo "No files to commit."
   exit 1
 fi
 
-# Loop through each file in the directory
+# loop through each file in the dir
 for file in *; do
-  # Check if it is a file (and not a directory)
   if [ -f "$file" ]; then
     # Add the file to the staging area
     git add "$file"
     
-    # Commit the file with a specific commit message
     git commit -m "Added file $file"
     
-    # Print a message indicating the file has been committed
     echo "Committed $file"
   fi
 done

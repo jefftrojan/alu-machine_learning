930f5b23876c715a44d6413e0af73b4465f605b6
jefftrojan <j.dauda@alustudent.com>
2024-05-25 16:28:54 +0200

    Added file 2-forward_prop.py

---
+++ supervised_learning/tensorflow/2-forward_prop.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+""" create layer """
+
+
+import tensorflow as tf
+create_layer = __import__('1-create_layer').create_layer
+
+
+def forward_prop(x, layer_sizes=[], activations=[]):
+    """ forward prop"""
+    L = x
+    for i in range(len(layer_sizes)):
+        L = create_layer(L, layer_sizes[i],
+                         activations[i])
+    return L

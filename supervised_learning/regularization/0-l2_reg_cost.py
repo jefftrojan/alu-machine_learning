7e27496c7086705152f13f2c9f30ded727267659
jefftrojan <j.dauda@alustudent.com>
2024-05-31 17:37:51 +0200

    Fix validationn error L2 reg cost

--- supervised_learning/regularization/0-l2_reg_cost.py
+++ supervised_learning/regularization/0-l2_reg_cost.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 """L2 Regularization Cost"""
 import numpy as np
+
+
 def l2_reg_cost(cost, lambtha, weights, L, m):
     """Calculates the cost of a neural network with L2 regularization.
 

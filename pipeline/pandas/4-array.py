13f7d9631fc2eef3b88f87f0568794d1eb219388
jefftrojan <j.dauda@alustudent.com>
2024-06-23 17:45:13 +0200

    pep validation err

--- pipeline/pandas/4-array.py
+++ pipeline/pandas/4-array.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
-
+'''
+    Take the last 10 rows of the columns High
+    and Close and convert them into a numpy.ndarray
+'''
 import pandas as pd
 from_file = __import__('2-from_file').from_file
 
 df = from_file('../Data/coinbaseUSD_1-min_data_2014-12-01_to_2019-01-09.csv', ',')
 
-#  Rename  Timestamp to Datetime
-df = df.rename(columns={'Timestamp': 'Datetime'})
+A = df.iloc[-10:, [3, 4]].values
 
-print(df.tail())
+print(A)

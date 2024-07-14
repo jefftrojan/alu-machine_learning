002665bc552e955da1f114ca99b423b05c5f87d0
jefftrojan <j.dauda@alustudent.com>
2024-07-14 18:01:09 +0200

    14-country_users.sql

---
+++ pipeline/databases/14-country_users.sql
@@ -0,0 +1,7 @@
+-- create table 
+CREATE TABLE IF NOT EXISTS users (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
+);

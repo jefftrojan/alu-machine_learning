4ecfc42edda0f00f2dfe50ebd4c67f8be743f24c
jefftrojan <j.dauda@alustudent.com>
2024-07-14 18:01:08 +0200

    12-rating_genres.sql

---
+++ pipeline/databases/12-rating_genres.sql
@@ -0,0 +1,9 @@
+-- nested join
+SELECT tv_genres.name, SUM(tv_show_ratings.rate) AS rating
+FROM tv_genres
+INNER JOIN tv_show_genres
+ON tv_genres.id = tv_show_genres.genre_id
+INNER JOIN tv_show_ratings
+ON tv_show_genres.show_id = tv_show_ratings.show_id
+GROUP BY name
+ORDER BY rating DESC;

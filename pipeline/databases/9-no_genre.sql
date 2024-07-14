02fc4202287461b0dcf2355d9da10d44bae31320
jefftrojan <j.dauda@alustudent.com>
2024-07-14 18:01:11 +0200

    9-no_genre.sql

---
+++ pipeline/databases/9-no_genre.sql
@@ -0,0 +1,6 @@
+-- join
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_show_genres.genre_id IS NULL
+ORDER BY title ASC, tv_show_genres.genre_id ASC

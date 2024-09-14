87370c95a509b4f44d984adbaf9efc21ee7501d4
jefftrojan <j.dauda@alustudent.com>
2024-09-14 23:11:14 +0200

    8-bi_rnn.py

---
+++ supervised_learning/RNNs/8-bi_rnn.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Defines function that performs forward propagation for bidirectional RNN
+"""
+
+
+import numpy as np
+
+
+def bi_rnn(bi_cell, X, h_0, h_t):
+    """
+    Performs forward propagation for bidirectional RNN
+    """
+    return None, None

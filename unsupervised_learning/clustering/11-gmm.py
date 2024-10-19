ffcd83d438b3f479c0619506700b11a470e51b01
jefftrojan <j.dauda@alustudent.com>
2024-10-19 17:06:35 +0200

    11-gmm.py

--- unsupervised_learning/clustering/11-gmm.py
+++ unsupervised_learning/clustering/11-gmm.py
@@ -1,35 +1,26 @@
 #!/usr/bin/env python3
-"""
-Defines function that calculates a GMM from a dataset
-"""
-
-
+"""This module contains a function that perfoms
+calculates a GMM from a dataset"""
 import sklearn.mixture
 
 
 def gmm(X, k):
     """
-    Calculates a GMM from a dataset
-
-    parameters:
-        X [numpy.ndarray of shape (n, d)]:
-            contains the dataset that will be used for K-means clustering
-            n: the number of data points
-            d: the number of dimensions for each data point
-        k [positive int]:
-            contains the number of clusters
-
-    returns:
-        pi, m, S, clss, bic:
-            pi [numpy.ndarray of shape (k,)]:
-                containing the cluster priors
-            m [numpy.ndarray of shape (k, d)]:
-                containing the centroid means
-            S [numpy.ndarray of shape (k, d, d)]:
-                containing the covariance matrices
-            clss [numpy.ndarray of shape (n,)]:
-                containting the cluster indices for each data point
-            bic [numpy.ndarray of shape (kmax - kmin + 1)]:
-                containting the BIC value for each cluster size tested
+    calculates a GMM from a dataset
+        X is a numpy.ndarray of shape (n, d) containing the dataset
+        k is the number of clusters
+    Returns: pi, m, S, clss, bic
+        pi: is a numpy.ndarray of shape (k,) containing the cluster priors
+        m: is a numpy.ndarray of shape (k, d) containing the centroid means
+        S: is a numpy.ndarray of shape (k, d, d) containing the covariance
+            matrices
+        clss: is a numpy.ndarray of shape (n,) containing the cluster indices
+            for each data point
+        bic: is a numpy.ndarray of shape (kmax - kmin + 1) containing the BIC
+            value for each cluster size tested
     """
-    return None, None, None, None, None
+    Gmm = sklearn.mixture.GaussianMixture(k)
+    params = Gmm.fit(X)
+    clss = Gmm.predict(X)
+    return (params.weights_, params.means_,
+            params.covariances_, clss, Gmm.bic(X))

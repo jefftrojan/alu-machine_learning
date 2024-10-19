1c4fbdb5fc90a98bb77032310510c5e4541ff7fb
jefftrojan <j.dauda@alustudent.com>
2024-10-19 17:06:35 +0200

    3-optimum.py

--- unsupervised_learning/clustering/3-optimum.py
+++ unsupervised_learning/clustering/3-optimum.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
-"""
-Defines function that tests for the optimum number of clusters by variance
-"""
 
+"""
+This module contains a function that
+tests for the optimum number of clusters by variance
+"""
 
 import numpy as np
 kmeans = __import__('1-kmeans').kmeans
@@ -11,31 +12,37 @@ variance = __import__('2-variance').variance
 
 def optimum_k(X, kmin=1, kmax=None, iterations=1000):
     """
-    Tests for the optimum number of clusters by variance
+    calculates intra-cluster variance for a dataset
 
-    parameters:
-        X [numpy.ndarray of shape (n, d)]:
-            contains the dataset used for K-means clustering
-            n: the number of data points
-            d: the number of dimensions for each data point
-        kmin [positive int]:
-            containing the minimum number of clusters to check for (inclusive)
-        kmax [positive int]:
-            containing the maximum number of clusters to check for (inclusive)
-        iterations [positive int]:
-            containing the maximum number of iterations for K-means
+    X: numpy.ndarray (n, d) containing the dataset
+        - n no. of data points
+        - d no. of dimensions for each data point
+    kmin: positive integer - the minimum no. of clusters
+    kmax: positive integer - the maximum no. of clusters
+    iterations: +ve(int) - max no. of iterations perfomed
 
-    function should analyze at least 2 different cluster sizes
-
-    should use at most 2 loops
-
-    returns:
-        results, d_vars:
-            results [list]:
-                containing the output of K-means for each cluster size
-            d_vars [list]:
-                containing the difference in variance from the smallest cluster
-                    size for each cluster size
-        or None, None on failure
+    return:
+        - results: list containing the results of the
+        K-means for each cluster size
+        - d_vars: list containing the difference in variance
+        from the smallest cluster size for each cluster size
     """
-    return None, None
+    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
+        return None, None
+    if not isinstance(kmin, int) or kmin <= 0:
+        return None, None
+    if not isinstance(kmax, int) or kmax <= 0:
+        return None, None
+    if kmin >= kmax:
+        return None, None
+    if not isinstance(iterations, int) or iterations <= 0:
+        return None, None
+    results = []
+    d_vars = []
+    for k in range(kmin, kmax + 1):
+        C, clss = kmeans(X, k, iterations)
+        results.append((C, clss))
+        if k == kmin:
+            var = variance(X, C)
+        d_vars.append(var - variance(X, C))
+    return results, d_vars

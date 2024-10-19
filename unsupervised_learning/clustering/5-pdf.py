0239ccfdb7bf3e1a794610d6e0f44269f373b514
jefftrojan <j.dauda@alustudent.com>
2024-10-19 17:06:36 +0200

    5-pdf.py

--- unsupervised_learning/clustering/5-pdf.py
+++ unsupervised_learning/clustering/5-pdf.py
@@ -1,34 +1,40 @@
 #!/usr/bin/env python3
-"""
-Defines function that calculates the probability density function of
-a Gaussian distribution
-"""
 
+"""
+This module contains a function that calculates
+probability density function of a Gaussian distribution
+"""
 
 import numpy as np
 
 
 def pdf(X, m, S):
     """
-    Calculates the probability density function of a Gaussian distribution
+    initializes variables for a Gaussian Mixture Model
 
-    parameters:
-        X [numpy.ndarray of shape (n, d)]:
-            contains the dataset whose PDF should be calculated
-            n: the number of data points
-            d: the number of dimensions for each data point
-        m [numpy.ndarray of shape (d,)]:
-            contains the mean of the distribution
-        S [numpy.ndarray of shape (d, d)]:
-            contains the covariance of the distribution
+    X: numpy.ndarray (n, d) containing the dataset
+        - n no. of data points
+        - d no. of dimensions for each data point
+    m: numpy.ndarray (d,) mean of the distribution
+    S: numpy.ndarray (d, d) covariance matrix of the distribution
 
-    not allowed to use any loops
-    not allowed to use the function numpy.diag or method numpy.ndarray.diagonal
-
-    returns:
-        P [numpy.ndarray of shape (n,)]:
-            containing the PDF values for each data point
-            all values in P should have a minimum value of 1e-300
-        or None on failure
+    return:
+        - P: numpy.ndarray (n,) the PDF values for each data point
     """
-    return None
+    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
+        return None
+    if not isinstance(m, np.ndarray) or len(m.shape) != 1:
+        return None
+    if not isinstance(S, np.ndarray) or len(S.shape) != 2:
+        return None
+    n, d = X.shape
+    if d != m.shape[0] or d != S.shape[0] or d != S.shape[1]:
+        return None
+    S_det = np.linalg.det(S)
+    S_inv = np.linalg.inv(S)
+    fac = 1 / np.sqrt(((2 * np.pi) ** d) * S_det)
+    X_m = X - m
+    X_m_dot = np.dot(X_m, S_inv)
+    X_m_dot_X_m = np.sum(X_m_dot * X_m, axis=1)
+    P = fac * np.exp(-0.5 * X_m_dot_X_m)
+    return np.maximum(P, 1e-300)

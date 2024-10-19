4e13694249afd0e88fc4024e0c332b4ca59052c9
Trojan0x <j.dauda@alustudent.com>
2024-10-19 17:27:14 +0200

    Update 4-initialize.py

--- unsupervised_learning/clustering/4-initialize.py
+++ unsupervised_learning/clustering/4-initialize.py
@@ -1,9 +1,5 @@
 #!/usr/bin/env python3
-
-"""
-This module contains a function that
-initializes variables for a Gaussian Mixture Model
-"""
+"""GMM function """
 
 import numpy as np
 kmeans = __import__('1-kmeans').kmeans
@@ -11,27 +7,34 @@ kmeans = __import__('1-kmeans').kmeans
 
 def initialize(X, k):
     """
-    initializes variables for a Gaussian Mixture Model
-
-    X: numpy.ndarray (n, d) containing the dataset
-        - n no. of data points
-        - d no. of dimensions for each data point
-    k: positive integer - the number of clusters
-
-    return:
-        - pi: numpy.ndarray (k,) containing priors for each cluster
-        initialized to be equal
-        - m: numpy.ndarray (k, d) containing centroid means for each cluster,
-        initialized with K-means
-        - S: numpy.ndarray (k, d, d) covariance matrices for each cluster,
-        initialized as identity matrices
+    initializes variables a Gaussian Mixture Model
+    Args:
+        X: numpy.ndarray of shape (n, d) containing the data set
+        k: positive integer containing the number of clusters
+    Returns: pi, m, S, or None, None, None on failure
+            pi: numpy.ndarray of shape (k,) containing the priors
+                each cluster initialized evenly
+            m: numpy.ndarray of shape (k, d) containing the centroid
+                means for each cluster, initialized with K-means
+            s: numpy.ndarray of shape (k, d, d) containing the covariance
+               matrices each cluster, initialized as identity matrices
     """
-    n, d = X.shape
+
     if not isinstance(X, np.ndarray) or len(X.shape) != 2:
         return None, None, None
-    if not isinstance(k, int) or k <= 0:
+    if not isinstance(k, int) or k < 1:
         return None, None, None
-    pi = np.full((k,), 1 / k)
+
+    n, d = X.shape
+
+    # priors each cluster, initialized evenly
+    phi = np.ones(k)/k
+
+    # centroid means each cluster, initialized with K-means
     m, _ = kmeans(X, k)
-    S = np.full((k, d, d), np.identity(d))
-    return pi, m, S
+
+    # covariance matrices each cluster, initialized as identity matrices
+    S = np.tile(np.identity(d), (k, 1)).reshape(k, d, d)
+
+    return phi, m, S
+    

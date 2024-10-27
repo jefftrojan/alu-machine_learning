773875ca72aa01c5e2e6efc43bd056f7afdbea66
jefftrojan <j.dauda@alustudent.com>
2024-10-27 09:41:27 +0200

    pep val

--- unsupervised_learning/hyperparameter_tuning/4-bayes_opt.py
+++ unsupervised_learning/hyperparameter_tuning/4-bayes_opt.py
@@ -1,70 +1,65 @@
 #!/usr/bin/env python3
-
 """
-This script represents a Bayesian optimization on a
-noiseless 1D Gaussian process
+4. Bayesian Optimization - Acquisition
 """
-
 import numpy as np
+from scipy.stats import norm
 GP = __import__('2-gp').GaussianProcess
+
 
 class BayesianOptimization:
     """
-    A class that represents a Bayesian optimization on a
-    noiseless 1D Gaussian process
+    Performs Bayesian optimization on a noiseless 1D Gaussian process
     """
-    def __init__(self, f, X_init, Y_init, bounds, ac_samples, l=1, sigma_f=1, xsi=0.01, minimize=True):
+
+    def __init__(self, f, X_init, Y_init, bounds, ac_samples, l=1,
+                 sigma_f=1, xsi=0.01, minimize=True):
         """
-        Initializes Bayesian Optimization
+        Class constructor
+        Args:
+            f: black-box function to be optimized
+            X_init: np.ndarray - (t, 1) - inputs already sampled with the
+                black-box function
+            Y_init: np.ndarray - (t, 1) - outputs of the black-box function
+                for each input in X_init
+            bounds: tuple (min, max) - bounds of the space in which to look
+                for the optimal point
+            ac_samples: number of samples that should be analyzed during
+                acquisition
+            l: length parameter for the kernel
+            sigma_f: standard deviation given to the output of the
+                black-box function
+            xsi: exploration-exploitation factor for acquisition
+            minimize: bool determining whether optimization should be
+                performed for minimization (True) or maximization (False)
         """
+        MIN, MAX = bounds
+
         self.f = f
-        self.gp = GP(X_init, Y_init, l, sigma_f)
-        self.X_s = np.linspace(bounds[0], bounds[1], ac_samples).reshape(-1, 1)
+        self.gp = GP(X_init, Y_init, l=l, sigma_f=sigma_f)
+        self.X_s = np.linspace(MIN, MAX, num=ac_samples)[..., np.newaxis]
         self.xsi = xsi
         self.minimize = minimize
 
     def acquisition(self):
         """
         Calculates the next best sample location
+        Uses the Expected Improvement acquisition function
+        Returns: X_next, EI
         """
-        mu, sigma = self.gp.predict(self.X_s)
-        sigma = sigma.reshape(-1, 1)
-        with np.errstate(divide='warn'):
-            if self.minimize:
-                mu_sample_opt = np.min(self.gp.Y)
-                imp = mu_sample_opt - mu - self.xsi
-            else:
-                mu_sample_opt = np.max(self.gp.Y)
-                imp = mu - mu_sample_opt - self.xsi
-            Z = imp / sigma
-            ei = imp * self._cdf(Z) + sigma * self._pdf(Z)
-            ei[sigma == 0.0] = 0.0
-        return self.X_s[np.argmax(ei)], ei
+        mu, _ = self.gp.predict(self.gp.X)
+        sample_mu, sigma = self.gp.predict(self.X_s)
 
-    def _cdf(self, Z):
-        """
-        Computes the cumulative distribution function (CDF) for standard normal distribution
-        """
-        return 0.5 * (1 + np.erf(Z / np.sqrt(2)))
-
-    def _pdf(self, Z):
-        """
-        Computes the probability density function (PDF) for standard normal distribution
-        """
-        return (1 / np.sqrt(2 * np.pi)) * np.exp(-0.5 * Z**2)
-
-    def optimize(self, iterations=100):
-        """
-        Optimizes the black-box function
-        """
-        X_all = []
-        for _ in range(iterations):
-            X_next, _ = self.acquisition()
-            Y_next = self.f(X_next)
-            self.gp.update(X_next, Y_next)
-            X_all.append(X_next)
         if self.minimize:
-            idx = np.argmin(self.gp.Y)
+            opt_mu = np.min(mu)
         else:
-            idx = np.argmax(self.gp.Y)
-        return self.gp.X[idx], self.gp.Y[idx]
+            opt_mu = np.max(mu)
+
+        imp = opt_mu - sample_mu - self.xsi
+        Z = imp / sigma
+        EI = ((imp * norm.cdf(Z)) + (sigma * norm.pdf(Z)))
+        EI[sigma == 0.0] = 0.0
+
+        X_next = self.X_s[np.argmax(EI)]
+
+        return X_next, np.array(EI)
